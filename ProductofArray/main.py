cb6b8d35d369711438b52230ec98ed4ba570d00c
Nick Bobertz <Nbobertz@gmail.com>
2024-08-01 19:22:15 -0400

    did the medium my janky way

--- ProductofArray/main.py
+++ ProductofArray/main.py
@@ -2,23 +2,76 @@
 
 #had to watch neetcode for the solution. The trick here is to first multiply by the left and thne the right. After this multiply by each last value and you have the answer
 
+#below is neetcode answre
+
 #below is test case
-nums = [1,2,3,4]
+nums = [2,3,5,0]
 
-def solution():
-    res =[1]*(len(nums))
+# def solution():
+#     res =[1]*(len(nums))
+#
+#     #for the prefix multiple
+#     prefix = 1
+#     for i in range(len(nums)):
+#         res[i]=prefix
+#         prefix *=nums[i]
+#     postfix = 1
+#     for i in range(len(nums)-1,-1,-1):
+#         res[i]*=postfix
+#         postfix *= nums[i]
+#
+#     return res
 
-    #for the prefix multiple
-    prefix = 1
-    for i in range(len(nums)):
-        res[i]=prefix
-        prefix *=nums[i]
-    postfix = 1
-    for i in range(len(nums)-1,-1,-1):
-        res[i]*=postfix
-        postfix *= nums[i]
+def solution2():
+    #here we are going to compute the pre and post fix for the above
+    answer = []
+    print(nums)
 
-    return res
+    #below is going to the be prefix
+    prefix=[]
+    prefixnum=0
+    for p1 in range(0,len(nums)):
+        if p1 == 0:
+            prefixnum=1
+            prefix.append(nums[p1]*prefixnum)
+        else:
+            prefix.append(nums[p1]*prefix[p1-1])
+    print(prefix)
 
-answer=solution()
+    #now we are going to do postfix
+    postfix = []
+    postfixnum = 0
+    counter = 0
+    for p1 in range(0,len(nums)):
+        counter+=-1
+        if counter == -1:
+            postfixnum = 1
+            postfix.insert(0,nums[counter]*postfixnum)
+        else:
+            postfix.insert(0,nums[counter]*postfix[counter+1])
+    print(postfix)
+    #now we are going to multiply the nums index by the pre and postfix and then insert it into our final product
+
+    for p1 in range(0,len(nums)):
+        finalnum = 0
+        prestore=0
+        poststore=0
+        if p1 == 0:
+            prestore=1
+            poststore=postfix[p1+1]
+            finalnum =prestore*poststore
+            answer.append(finalnum)
+        elif p1 == len(nums)-1:
+            poststore = 1
+            prestore=prefix[p1-1]
+            finalnum = prestore*poststore
+            answer.append(finalnum)
+        else:
+            poststore=postfix[p1+1]
+            prestore=prefix[p1-1]
+            finalnum = prestore*poststore
+            answer.append(finalnum)
+    return answer
+
+answer=solution2()
 print(answer)
