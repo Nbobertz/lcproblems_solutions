065b1261e121a026866a176e4adae7a0f77f7f19
Nick Bobertz <Nbobertz@gmail.com>
2024-09-22 21:26:28 -0400

    did medium

--- restofLeetcode/threesum.py
+++ restofLeetcode/threesum.py
@@ -13,7 +13,52 @@ def solution():
     l,r = 0,len(nums)-1
 
     #now we loop through the array and add to the hmap dict
-    while l<r:
+    while l<=r:
         #we add the element with the index point
-        hmap.update({nums[l]:l})
-        hmap.update({nums[r]:r})
+        hmap.update({l:nums[l]})
+        hmap.update({r:nums[r]})
+        l+=1
+        r-=1
+
+    #now we are going to loop through the hashmap and remove all key's which together add up to 0:
+
+    for p1 in range(0,len(hmap)):
+        for p2 in range(p1+1,len(hmap)):
+            for p3 in range(p2+1,len(hmap)):
+                if hmap[p1]+hmap[p2]+hmap[p3] == 0:
+                    answer.append([hmap[p1],hmap[p2],hmap[p3]])
+                    hmap['test']=hmap[p1]
+                    del hmap[p1]
+                    hmap['test']=hmap[p2]
+                    del hmap[p2]
+                    hmap['test']=hmap[p3]
+                    del hmap[p3]
+    #couldnt get it right on the first try. Continued to have problems with the pointer enumeration
+
+    return answer
+
+
+
+#below is neetcode's code. The above code I could not edit the dictionary in place. Neetcode just changed the a variable and sorted the list ahead of time.
+def solution2():
+    res = []
+    nums.sort()
+
+    for i,a in enumerate(nums):
+        if i>0 and a==nums[i-1]:
+            continue
+
+        l,r = i+1,len(nums)-1
+        while l<r:
+            thresum = a+nums[l]+nums[r]
+            if thresum>0:
+                r-=1
+            elif thresum<0:
+                l+=1
+            else:
+                res.append([a,nums[l],nums[r]])
+                l+=1
+                while nums[l]==nums[l-1] and l<r:
+                    l+=1
+    return res
+print(solution2())
