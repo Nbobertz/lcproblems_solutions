c3bf6421c44df470a4a90d81d16e3d392a3eb388
Nick Bobertz <Nbobertz@gmail.com>
2024-11-25 19:14:20 -0500

    did easy

---
+++ restofLeetcode/returnHello.py
@@ -0,0 +1,20 @@
+#easy problem. Build a .js function that can return hello world
+
+# / **
+# * @ return {Function}
+# * /
+# var
+# createHelloWorld = function()
+# {
+#
+# return function(...
+# args) {
+# return "Hello World";
+# }
+# };
+#
+# / **
+# *const
+# f = createHelloWorld();
+# *f(); // "Hello World"
+#          * /
