775f9c32d6577633fa9f4c73f6b86f2d5ccfba43
Nick Bobertz <Nbobertz@gmail.com>
2024-11-27 22:44:28 -0500

    did med

---
+++ restofLeetcode/construct-binary-tree.py
@@ -0,0 +1,21 @@
+#here we are given an inorder and preorder traversel array and the idea is that you need to construct a binary search tree out of these.
+
+#the trick comes down to recognising how the preorder root can be seen from the inorder center. Everything to the right is the right of the tree and the left is the left.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        #build root
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
