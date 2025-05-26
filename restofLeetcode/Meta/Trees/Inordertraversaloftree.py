2ebe8ca59a6c53be6148c10a8d302cb63ef9e455
Nick Bobertz <Nbobertz@gmail.com>
2025-05-26 10:58:41 -0400

    did easy

---
+++ restofLeetcode/Meta/Trees/Inordertraversaloftree.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
+def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    # recursive call to bottom, find bottom and append to res array value, go up and do it again.-Neetcode
+
+    res = []
+
+    def inorder(root):
+        if not root:
+            return  # base case caught
+
+        inorder(root.left)
+        res.append(root.val)
+        inorder(root.right)
+
+    inorder(root)
+    return res
