63f69101a004fd6ad66fe1c6186935e1b303787b
Nick Bobertz <Nbobertz@gmail.com>
2026-01-09 00:23:37 -0500

    did med

---
+++ restofLeetcode/DSA_Prep/BinarySearch/Guess_number_game.py
@@ -0,0 +1,34 @@
+"""
+This is the classic number game
+"""
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        if not n:
+            return None
+
+        if n == 1:
+            return 1
+
+        l,r = 0,n
+
+        while l<=r:
+            half = (l+r)//2
+
+            nguess = guess(num=half)
+            print(nguess)
+
+            #logic to move pointers
+            if nguess == -1:
+                r = half - 1
+            elif nguess == 1:
+                l = half + 1
+            elif nguess == 0:
+                return half
