ff588149236dc5f7929b81151da97390a6ac0fe5
Nick Bobertz <Nbobertz@gmail.com>
2026-07-19 18:13:09 -0400

    did easy

---
+++ restofLeetcode/DSA_Prep/DP/Stairs.py
@@ -0,0 +1,17 @@
+"""
+This is the classic counting stairs problem, here we want to count the number of ways we can reach the top of the staircase
+"""
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * n
+        def dfs(i):
+            if i >= n:
+                return i == n
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+
+        return dfs(0)
